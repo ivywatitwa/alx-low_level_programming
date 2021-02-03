1ef11ce72a10c2343cb563cbb7bdf8a71b12e4a1
IvyWatitwa <watitwaivy@gmail.com>
2021-02-03 15:36:34 +0000

    sizeof

---
+++ 0x00-hello/6-size.c
@@ -0,0 +1,21 @@
+#!/bin/bash
+#include <stdio.h>
+
+   /**
+    *main-programming starts here
+    *Return:0
+    *Description:Outputs size of 
+    */
+
+int main(void)
+
+{
+
+    printf("Size of a char: %d byte(s)\n", sizeof(char));
+printf("Size of an int: %d byte(s)\n", sizeof(int));
+printf("size of a long int: %d byte(s)\n", sizeof(long));
+printf("size of a long long int: %d byte(s)\n", sizeof(long long));
+printf("size of a float: %d byte(s)\n", sizeof(float));
+
+    return(0);
+}
