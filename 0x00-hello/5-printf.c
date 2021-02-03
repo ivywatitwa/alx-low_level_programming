0784b6e2dea65646a47f1ce5c40012b5ce22c16a
IvyWatitwa <watitwaivy@gmail.com>
2021-02-03 15:22:37 +0000

    printf

---
+++ 0x00-hello/5-printf.c
@@ -0,0 +1,11 @@
+#!/bin/bash
+#include <stdio.h>
+   /**
+    *main-programming starts here
+    *Return: 0
+    */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art ,\n");
+return(0);
+}
