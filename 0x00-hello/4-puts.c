eaab9a42c813caa7ef23fec301d215c470d329a4
IvyWatitwa <watitwaivy@gmail.com>
2021-02-03 15:16:22 +0000

    puts

---
+++ 0x00-hello/4-puts.c
@@ -0,0 +1,14 @@
+#!/bin/bash
+#include <stdio.h>
+
+   /**
+    *main -starting point of code
+    *Description: Outputs "\"Programming is like building a multilingual puzzle"
+    *Return: 0
+    */
+int main(void)
+
+{
+puts("\"Programming is like building a multilingual puzzle");
+return(0);
+}
