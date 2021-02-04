7702bb17f783ab06dacb435cc2e4bc470b80cdad
ivywatitwa <watitwaivy@gmail.com>
2021-02-04 20:07:25 +0000

    printing decimal

---
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+*main -prints decimal number system
+*
+*Return 0 on completion
+*/
+
+int main(void)
+{
+        int a, b;
+        for (a = 0; a < 10; a++)
+	{
+                for (b = 0; b < 10; b++)
+	        {
+	                putchar(a + '0');
+	                putchar(b + 'o');
+	                if (a == 9 && b == 9)
+		        {
+		                break;
+                        }
+	                        putchar(',');
+	                        putchar(' ');
+                }
+	}
+	putchar('\n');
+        return (0);
+}
