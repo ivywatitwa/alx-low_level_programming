c14ba01b2136cdd3bc87e54ea4cb3c37118b0c97
ivywatitwa <watitwaivy@gmail.com>
2021-02-04 19:02:07 +0000

    print numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+*
+*main-prints number
+*
+*Return : 0
+*/
+
+int main(void)
+{
+        int a;
+
+        for(a = 0; a < 10; a++)
+	        printf("%d", a);
+        printf("\n");
+        return (0);
+}
