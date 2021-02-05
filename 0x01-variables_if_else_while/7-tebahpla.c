fbfe01350983aa4ffe6b26ac49330ac6622f335a
ivywatitwa <watitwaivy@gmail.com>
2021-02-05 20:05:17 +0000

    prints reversed letters

--- 0x01-variables_if_else_while/7-tebahpla.c
+++ 0x01-variables_if_else_while/7-tebahpla.c
@@ -2,15 +2,16 @@
 #include <stdlib.h>
 
 /**
-*
-*main-prints in reverse
-*
-*Return 0 on success
-*/
+  *
+  *main-prints in reverse
+  *
+  *Return 0 on success
+  */
 
 int main(void)
 {
         char letter = 'z';
+
         while (letter != 'a' - 1)
 	{
                 putchar(letter);
