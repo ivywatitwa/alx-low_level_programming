dde4b551519929ffe5afd32e736ce930c137fa6a
ivywatitwa <watitwaivy@gmail.com>
2021-02-12 18:16:03 +0000

    C code

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 
 /**
-  *main-programming starts here
-  *Return: 0
+  *main-runs teh main code
+  *
+  *Return: 0 if program completes
   */
 int main(void)
 {
-        printf("with proper grammar, but the outcome is a piece of art,\n");
-        return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
+
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,22 +1,23 @@
 #include <stdio.h>
 
 /**
-  *main-outputs size of
+  *main-runs the main code
   *
-  *Return 0 on success 
-  */ 
+  *Return: 0 if program completes
+  */
 int main(void)
 {
-        char a;
-        int b;
-        long int c;
-        long long int d;
-        float e;
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float e;
 
-        printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
-        printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(b));
-        printf("size of a long int: %lu byte(s)\n", (unsigned long)sizeof(c));
-        printf("size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(d));
-	printf("size of a float: %lu byte(s)\n", (unsigned long)sizeof(e));
-        return (0);
+	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
+	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(b));
+	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(c));
+	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(d));
+	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(e));
+	return (0);
 }
+
